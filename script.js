6c77724dc1bf6ef3fadb72e3e671b9a9e0acee47
Tim Grillmeier <tlgrillmeier@gmail.com>
2023-04-15 14:53:06 +1000

    Tidied up js

--- script.js
+++ script.js
@@ -22,21 +22,12 @@ inputLightness.addEventListener('input', function(e) {
 
 function changeColor() {
   let hsl = "hsl("+ inputHueValue + ", " + inputSaturationValue + "%, " + inputLightnessValue + "%)"
-  // console.log("linear-gradient(to right, hsl("+ inputHueValue + ", " + 0 + "%, " + inputLightnessValue + "%) 0%, hsl("+ inputHueValue + ", " + 100 + "%, " + inputLightnessValue + "%) 100%);")
   let inputSatBGStr = "background: linear-gradient(to right, hsl("+ inputHueValue + ", 0%, 50%) 0%, hsl("+ inputHueValue + ", 100%, 50%) 100%);"
   inputSaturation.style = inputSatBGStr
-  console.log(inputSatBGStr)
   
   inputLightBGStr = "background: linear-gradient(to right, hsl("+ inputHueValue + ", 100%, 0%) 0%, hsl("+ inputHueValue + ", 100%, 50%), hsl("+ inputHueValue + ", 100%, 100%) 100%);"
   inputLightness.style = inputLightBGStr
-  console.log(inputLightBGStr)
 
-  
-
-  // console.log(hsl)
-  // inputHue.style.color = hsl
-  // inputHue.style.color = hsl
   document.body.style.background = hsl
 }
 
-changeColor()
